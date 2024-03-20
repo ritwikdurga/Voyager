aef32745a70a2418acc221b4a8b456378a4b4641
abhijitch1 <abhijitchunduri@gmail.com>
2024-03-20 17:29:35 +0530

    feat: Adds Reservation and attachments section in Overview Page

--- lib/pages/trip_planning_sections/main_tab_sections/overview.dart
+++ lib/pages/trip_planning_sections/main_tab_sections/overview.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:voyager/components/trip_planning_section/friends_icons.dart';
 import 'package:voyager/components/trip_planning_section/profile_tile.dart';
 
@@ -10,11 +12,11 @@ class OverviewTrips extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.fromLTRB(15, 10,15,0),
+          padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
           child: Row(
             children: [
               Text(
@@ -50,15 +52,123 @@ class OverviewTrips extends StatelessWidget {
           children: [
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.fromLTRB(8.0,0,8,8),
+                padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 8),
                 child: SizedBox(
                   height: 85,
-                  width:screenWidth-50,
+                  width: screenWidth - 50,
                   child: FriendsIcons(),
                 ),
               ),
             ),
           ],
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+          child: Row(
+            children: [
+              Text(
+                'Reservations and Attachments',
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 8),
+        SizedBox(
+          height: 80,
+          width: screenWidth,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                physics: ClampingScrollPhysics(),
+                children: [
+                  SizedBox(
+                    width: screenWidth / 4-15,
+                    height: 80,
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.flight_takeoff,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                        Spacer(),
+                        Text('Flights'),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0,0,0,18),
+                    child: VerticalDivider(
+                      thickness: 0.25,
+                    ),
+                  ),
+                  SizedBox(
+                    width: screenWidth / 4-15,
+                    height: 80,
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.train,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                        Spacer(),
+                        Text('Trains',),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0,0,0,18),
+                    child: VerticalDivider(
+                      thickness: 0.25,
+                    ),
+                  ),
+                  SizedBox(
+                    width: screenWidth / 4-15,
+                    height: 80,
+                    child: Column(
+                      children: [
+                        Icon(
+                          Iconsax.bus5,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                        Spacer(),
+                        Text('Buses',),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0,0,0,18),
+                    child: VerticalDivider(
+                      thickness: 0.25,
+                    ),
+                  ),
+                  SizedBox(
+                    width: screenWidth / 4-15,
+                    height: 80,
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.attachment_outlined,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                        Spacer(),
+                        Text('Attachment',),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -86,12 +196,13 @@ void _showBottomSheet(BuildContext context) {
           SizedBox(
             height: screenHeight / 2 - 10,
             child: ListView.builder(
-                itemCount: 100,
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                itemBuilder: (bc, index) {
-                  return ProfileTile();
-                }),
+              itemCount: 100,
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              itemBuilder: (bc, index) {
+                return ProfileTile();
+              },
+            ),
           ),
         ],
       );
