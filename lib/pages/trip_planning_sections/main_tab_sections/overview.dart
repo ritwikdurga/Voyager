c5e67880df663363928cf369af0f02332914ec3a
abhijitch1 <abhijitchunduri@gmail.com>
2024-03-21 16:45:10 +0530

    feat: Adds notes section in Overview page

--- lib/pages/trip_planning_sections/main_tab_sections/overview.dart
+++ lib/pages/trip_planning_sections/main_tab_sections/overview.dart
@@ -12,182 +12,317 @@ import 'package:voyager/components/trip_planning_section/profile_tile.dart';
 import 'package:voyager/home_screen.dart';
 import 'package:voyager/utils/constants.dart';
 
-class OverviewTrips extends StatelessWidget {
+class Item {
+  String heading;
+  String? notes;
+  bool isExpanded;
+  bool isEditing;
+  Item(
+      {required this.heading,
+      required this.notes,
+      this.isExpanded = false,
+      this.isEditing = false});
+}
+
+class OverviewTrips extends StatefulWidget {
   const OverviewTrips({super.key});
 
+  @override
+  State<OverviewTrips> createState() => _OverviewTripsState();
+}
+
+class _OverviewTripsState extends State<OverviewTrips> {
+  List<Item> notes = [Item(heading: 'Note 1', notes: null)];
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
-            child: Row(
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 30.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+              child: Row(
+                children: [
+                  Text(
+                    'Your Tripmates',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  Spacer(),
+                  GestureDetector(
+                    onTap: () {
+                      _showBottomSheet(context);
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                          'See All',
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
               children: [
-                Text(
-                  'Your Tripmates',
-                  style: TextStyle(
-                    fontSize: 17,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 8),
+                    child: SizedBox(
+                      height: 85,
+                      width: screenWidth - 50,
+                      child: FriendsIcons(),
+                    ),
                   ),
                 ),
-                Spacer(),
-                GestureDetector(
-                  onTap: () {
-                    _showBottomSheet(context);
-                  },
-                  child: Row(
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+              child: Row(
+                children: [
+                  Text(
+                    'Reservations and Attachments',
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8),
+            SizedBox(
+              height: 80,
+              width: screenWidth,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    physics: ClampingScrollPhysics(),
                     children: [
-                      Text(
-                        'See All',
-                        style: TextStyle(
-                          fontSize: 12,
+                      GestureDetector(
+                        onTap: () {
+                          AttachForFlights(context);
+                        },
+                        child: SizedBox(
+                          width: screenWidth / 4 - 15,
+                          height: 80,
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.flight_takeoff,
+                                size: 50,
+                                color: Colors.white,
+                              ),
+                              Spacer(),
+                              Text('Flights'),
+                            ],
+                          ),
                         ),
                       ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 10,
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 18),
+                        child: VerticalDivider(
+                          thickness: 0.25,
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 8),
-                  child: SizedBox(
-                    height: 85,
-                    width: screenWidth - 50,
-                    child: FriendsIcons(),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
-            child: Row(
-              children: [
-                Text(
-                  'Reservations and Attachments',
-                  style: TextStyle(
-                    fontSize: 17,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 8),
-          SizedBox(
-            height: 80,
-            width: screenWidth,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ListView(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  physics: ClampingScrollPhysics(),
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        AttachForFlights(context);
-                      },
-                      child: SizedBox(
+                      SizedBox(
                         width: screenWidth / 4 - 15,
                         height: 80,
                         child: Column(
                           children: [
                             Icon(
-                              Icons.flight_takeoff,
+                              Icons.train,
                               size: 50,
                               color: Colors.white,
                             ),
                             Spacer(),
-                            Text('Flights'),
+                            Text(
+                              'Trains',
+                            ),
                           ],
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 18),
-                      child: VerticalDivider(
-                        thickness: 0.25,
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 18),
+                        child: VerticalDivider(
+                          thickness: 0.25,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: screenWidth / 4 - 15,
-                      height: 80,
-                      child: Column(
-                        children: [
-                          Icon(
-                            Icons.train,
-                            size: 50,
-                            color: Colors.white,
-                          ),
-                          Spacer(),
-                          Text(
-                            'Trains',
-                          ),
-                        ],
+                      SizedBox(
+                        width: screenWidth / 4 - 15,
+                        height: 80,
+                        child: Column(
+                          children: [
+                            Icon(
+                              Iconsax.bus5,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                            Spacer(),
+                            Text(
+                              'Buses',
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 18),
-                      child: VerticalDivider(
-                        thickness: 0.25,
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 18),
+                        child: VerticalDivider(
+                          thickness: 0.25,
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: screenWidth / 4 - 15,
-                      height: 80,
-                      child: Column(
-                        children: [
-                          Icon(
-                            Iconsax.bus5,
-                            size: 50,
-                            color: Colors.white,
-                          ),
-                          Spacer(),
-                          Text(
-                            'Buses',
-                          ),
-                        ],
+                      SizedBox(
+                        width: screenWidth / 4 - 15,
+                        height: 80,
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.attachment_outlined,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                            Spacer(),
+                            Text(
+                              'Attachment',
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 18),
-                      child: VerticalDivider(
-                        thickness: 0.25,
-                      ),
-                    ),
-                    SizedBox(
-                      width: screenWidth / 4 - 15,
-                      height: 80,
-                      child: Column(
-                        children: [
-                          Icon(
-                            Icons.attachment_outlined,
-                            size: 50,
-                            color: Colors.white,
-                          ),
-                          Spacer(),
-                          Text(
-                            'Attachment',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Text('Notes'),
+                Spacer(),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      notes.add(
+                          Item(heading: 'Note ${notes.length + 1}', notes: null));
+                    });
+                  },
                 ),
               ],
             ),
-          ),
-        ],
+            ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  notes[index].isExpanded = isExpanded;
+                  if (isExpanded == false) {
+                    notes[index].isEditing = false;
+                  }
+                });
+              },
+              elevation: 0,
+              materialGapSize: 5,
+              children: notes.asMap().entries.map<ExpansionPanel>((entry) {
+                Item item = entry.value;
+                TextEditingController _controller =
+                    TextEditingController(text: item.heading);
+                bool _isEditing = item.isEditing;
+                TextEditingController _controllerForBody =
+                    TextEditingController();
+                if (item.notes != null) {
+                  _controllerForBody.text = item.notes!;
+                }
+                return ExpansionPanel(
+                  canTapOnHeader: true,
+                  backgroundColor: Colors.grey[800],
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: _isEditing
+                              ? TextFormField(
+                                  initialValue: item.heading,
+                                  textInputAction: TextInputAction.done,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10))),
+                                  ),
+                                  onFieldSubmitted: (value) {
+                                    setState(() {
+                                      _isEditing = false;
+                                      item.isEditing = false;
+                                      item.heading = value;
+                                    });
+                                  })
+                              : Text(
+                                  item.heading,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                        ),
+                        if (isExpanded)
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.edit),
+                                onPressed: () {
+                                  debugPrint('pressed me!');
+                                  setState(() {
+                                    _isEditing = !_isEditing;
+                                    item.isEditing = !item.isEditing;
+                                    debugPrint(_isEditing.toString());
+                                  });
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  setState(() {
+                                    notes.removeWhere((Item currentItem) =>
+                                        item == currentItem);
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                      ],
+                    );
+                  },
+                  body: TextField(
+                      controller: _controllerForBody,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10))),
+                        hintText:
+                            item.notes != null ? null : 'Tap to write notes',
+                      ),
+                      maxLines: null,
+                      onSubmitted: (value) {
+                        setState(() {
+                          item.notes = value;
+                        });
+                      }),
+                  isExpanded: item.isExpanded,
+                );
+              }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
