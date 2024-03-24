f85715d0745fcdeed6d4b906706c7bf334583561
ritwikdurga <142917963+ritwikdurga@users.noreply.github.com>
2024-03-24 12:48:30 +0530

    feat: adds bookmark animation to trip component

--- lib/components/explore_section/trips_cards.dart
+++ lib/components/explore_section/trips_cards.dart
@@ -1,21 +1,43 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import "package:flutter/material.dart";
+import "package:lottie/lottie.dart";
 import "package:provider/provider.dart";
 import "package:voyager/utils/constants.dart";
 
 import "../../utils/colors.dart";
 
-class trips extends StatelessWidget {
+class trips extends StatefulWidget {
   trips({
     super.key,
     required this.screenWidth,
   });
 
+  final double screenWidth;
+
+  @override
+  State<trips> createState() => _tripsState();
+}
+
+class _tripsState extends State<trips> with SingleTickerProviderStateMixin {
+  bool isBookmarked = false;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 2300));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   final ShapeBorder shape = RoundedRectangleBorder(
     borderRadius: BorderRadius.all(Radius.circular(20)),
   );
-  final double screenWidth;
 
   @override
   Widget build(BuildContext context) {
@@ -36,15 +58,15 @@ class trips extends StatelessWidget {
           // redirect to planning page.
         },
         child: SizedBox(
-          height: screenWidth / 3 + 20,
-          width: screenWidth - 10,
+          height: widget.screenWidth / 3 + 20,
+          width: widget.screenWidth - 10,
           child: Stack(
             children: [
               Image.asset(
                 'assets/images/a.png',
                 fit: BoxFit.cover,
-                height: screenWidth / 3 + 20,
-                width: screenWidth - 10,
+                height: widget.screenWidth / 3 + 20,
+                width: widget.screenWidth - 10,
               ),
               Padding(
                 padding: const EdgeInsets.all(20.0),
@@ -52,13 +74,37 @@ class trips extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      '31 Mar - 4 Apr',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 14,
-                        color: Colors.white,
-                      ),
+                    Row(
+                      children: [
+                        Text(
+                          '31 Mar - 4 Apr',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Spacer(),
+                        GestureDetector(
+                          onTap: () {
+                            if (isBookmarked) {
+                              _controller.reverse();
+                              isBookmarked = false;
+                            } else {
+                              isBookmarked = true;
+                              _controller.forward();
+                            }
+                          },
+                          child: SizedBox(
+                            height: 40,
+                            width: 40,
+                            child: Lottie.network(
+                              'https://lottie.host/bee59ff8-eb4a-4245-9e8d-ad6521f98bf1/zFNurdNhaS.json',
+                              controller: _controller,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                     Spacer(),
                     Text(
@@ -71,7 +117,6 @@ class trips extends StatelessWidget {
                       overflow: TextOverflow.ellipsis,
                       maxLines: 1,
                     ),
-                    
                   ],
                 ),
               ),
